5bfde17db76da0e24703db2d9c5e424db1b154bc
Nguyentrang197 <thutranglop92@gmail.com>
2019-07-03 10:48:14 +0700

    BE-5 Return profile's information

--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,6 @@ use Illuminate\Http\Request;
 
     Route::group(['middleware' => 'auth:api'], function() {
         //Must login and use access_token to access these route.
-
         Route::get('logout', 'AuthController@logout');
         Route::get('profile', 'AuthController@user');
     });
