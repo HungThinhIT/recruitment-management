accd7c08b546f38767508c67153595441924c2f9
Nguyentrang197 <thutranglop92@gmail.com>
2019-07-10 13:30:26 +0700

    Update seeder

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,6 +11,17 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+    	DB::table('users')->insert([
+        	'name' => 'admin',
+	        'email' => 'admin@gmail.com',
+	        'email_verified_at' => now(),
+	        'password' => Hash::make(123123), 
+	        'remember_token' => Str::random(10),
+	        'fullname'=>'Admin',
+	        'phone'=>'0123456789',
+	        'address'=>'453-455 Hoàng Diệu, TP Đà Nẵng',
+	        'image'=>'avt_default_profile.png',
+        ]);
         factory(User::class, 5)->create();
     }
 }
