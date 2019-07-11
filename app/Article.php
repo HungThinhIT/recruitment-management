b220a3dfdc86b29a96b22eed02438170924ed2fd
HungThinh0710 <nhtnokia@gmail.com>
2019-07-11 16:10:05 +0700

    Update API for return a job information

--- app/Article.php
+++ app/Article.php
@@ -8,6 +8,7 @@ class Article extends Model
 {
     protected $table = 'articles';
     protected $primaryKey = 'id';
+
 	public function user()
     {
         return $this->belongsTo('App\User','userId');
