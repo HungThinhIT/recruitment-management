1acd6767327ea1014b4aac834ce2e05be4f96aa6
Nguyễn Hưng Thịnh <33023931+HungThinh0710@users.noreply.github.com>
2019-07-23 20:41:48 +0700

    Revert "[BE-46] Create the API to show list interview"

--- app/Interview.php
+++ app/Interview.php
@@ -6,14 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Interview extends Model
 {
-
     protected $table = 'interviews';
     protected $primaryKey = 'id';
-    public $fillable = ["name", "timeStart", "address", "interviewerId"];
-
-    public function scopeFilter($query, QueryFilter $filters)
-    {
-        return $filters->apply($query);
-    }
-
 }
